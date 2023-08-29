c22f3ceb4bd750535b6d36e3f622ac02d10a4809
Uriel Milgron <urielmilgron18@gmail.com>
2023-08-28 22:10:00 -0300

    itemList and flatList Added

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [textValue, setTextValue] = useState('');
+  const [itemList, setItemList] = useState([])
+
+  const onHandleChangeItem = text => setTextValue(text)
+  const onSendItem = () => setItemList(prevState => [...prevState,{id:Math.random(), value:textValue}])
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.inputContainer}>
-      <TextInput style={styles.input} placeholder='Ingrese un item'/>
-      
-   
-      <Button title='Añadir'></Button>
+      <TextInput style={styles.input} placeholder='Ingrese un item' value={textValue} onChangeText={onHandleChangeItem}/>
+      <Button style={styles.buttonAdd} onPress={onSendItem} title='Añadir'></Button>
+      </View>
+      <View>
+      <FlatList data={itemList} renderItem={({item}) => (<Text>{item.value}</Text>)} keyExtractor={item => item.id}/>
       </View>
     </View>
   );
@@ -26,5 +33,10 @@ const styles = StyleSheet.create({
   },
   input:{
     width:200,
+  },
+  buttonAdd:{
+    color:"#ff0000"
   }
 });
+//Los nombres que de estilos que tienen que ver
+//con textos los puso con mayúsculas
